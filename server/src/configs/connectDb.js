de64bac00ea27474d7c83ec6238bd4db0901eb7e
hhoang777 <hhoang1072003@gmail.com>
2025-01-23 18:50:14 +0700

    connectDb production

--- server/src/configs/connectDb.js
+++ server/src/configs/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.CONNECTION_STRING);
+    await mongoose.connect(process.env.CONNECTION_STRING_PRODUCTION);
     console.log("Kết nối MongoDB thành công ✅ ");
   } catch (error) {
     console.error(" Lỗi kết nối MongoDB ❌: " + err);
